507e7d4abf99cc93c9335577d310bd22f2ebd503
tranhoanglon98 <you@example.com>
2022-07-13 19:46:50 +0700

    update exercise

--- ss1_database/exercise/student_management.sql
+++ ss1_database/exercise/student_management.sql
@@ -1,5 +1,7 @@
 create database student_management;
+
 use student_management;
+
 create table student(
 id_student int,
 `name` varchar(55),
@@ -7,9 +9,15 @@ age int,
 country varchar(55)
 );
 
-alter table student add primary key (id_student);
-alter table student change column id_student id_student int AUTO_INCREMENT;
-insert into student (`name`,age,country) value ("long",32,"Việt Nam"),("Phú",23,"lào");
+alter table student 
+add primary key (id_student);
+
+alter table student 
+change column id_student id_student int AUTO_INCREMENT;
+
+insert into student (`name`,age,country) 
+value ("long",32,"Việt Nam"),("Phú",23,"lào");
+
 select*from student;
 
 create table class(
