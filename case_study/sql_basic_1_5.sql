f62fc2022708d18bc262bf0df6cdc00f7f243370
tranhoanglon98 <you@example.com>
2022-07-17 12:30:11 +0700

    sql basic 1 -5

---
+++ case_study/sql_basic_1_5.sql
@@ -0,0 +1,67 @@
+USE furama_resort;
+
+-- Hiển thị thông tin của tất cả nhân viên có tên bắt đầu là một trong các ký tự “H”, “T” hoặc “K” và có tối đa 15 kí tự.
+SELECT 
+    *
+FROM
+    nhan_vien
+WHERE
+    ho_ten REGEXP '^[HTK]'
+        AND char_length(ho_ten) <= 15;
+
+
+-- Hiển thị thông tin của tất cả khách hàng có độ tuổi từ 18 đến 50 tuổi và có địa chỉ ở “Đà Nẵng” hoặc “Quảng Trị”. 
+SELECT 
+    *
+FROM
+    khach_hang
+WHERE
+    (YEAR(CURDATE()) - YEAR(ngay_sinh) - (RIGHT(CURDATE(), 5) < RIGHT(ngay_sinh, 5))) <= 50
+        AND (YEAR(CURDATE()) - YEAR(ngay_sinh) - (RIGHT(CURDATE(), 5) < RIGHT(ngay_sinh, 5))) >= 18
+        AND (dia_chi REGEXP 'Đà Nẵng$'
+        OR dia_chi REGEXP 'Quảng Trị$');
+        
+
+-- Đếm xem tương ứng với mỗi khách hàng đã từng đặt phòng bao nhiêu lần.
+-- Kết quả hiển thị được sắp xếp tăng dần theo số lần đặt phòng của khách hàng. 
+-- Chỉ đếm những khách hàng nào có Tên loại khách hàng là “Diamond”. 
+SELECT 
+    kh.ma_khach_hang,
+    kh.ho_ten,
+    COUNT(hd.ma_khach_hang) AS so_lan_dat_phong
+FROM
+    khach_hang kh
+        JOIN
+    hop_dong hd ON kh.ma_khach_hang = hd.ma_khach_hang
+        JOIN
+    loai_khach lk ON kh.ma_loai_khach = lk.ma_loai_khach
+WHERE
+    lk.ten_loai_khach = 'Diamond'
+GROUP BY kh.ma_khach_hang
+ORDER BY so_lan_dat_phong;
+
+
+-- Hiển thị ma_khach_hang, ho_ten, ten_loai_khach, ma_hop_dong, ten_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc, tong_tien.
+SELECT 
+    kh.ma_khach_hang,
+    kh.ho_ten,
+    lk.ten_loai_khach,
+    hd.ma_hop_dong,
+    dv.ten_dich_vu,
+    hd.ngay_lam_hop_dong,
+    hd.ngay_ket_thuc,
+    (dv.chi_phi_thue + IFNULL(hdct.so_luong * dvdk.gia, 0)) AS tong_tien
+FROM
+    khach_hang kh
+        LEFT JOIN
+    hop_dong hd ON kh.ma_khach_hang = hd.ma_khach_hang
+        LEFT JOIN
+    hop_dong_chi_tiet hdct ON hdct.ma_hop_dong = hd.ma_hop_dong
+        LEFT JOIN
+    dich_vu_di_kem dvdk ON dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+        LEFT JOIN
+    dich_vu dv ON dv.ma_dich_vu = hd.ma_dich_vu
+        LEFT JOIN
+    loai_khach lk ON lk.ma_loai_khach = kh.ma_loai_khach
+GROUP BY hd.ma_hop_dong,kh.ma_khach_hang
+ORDER BY kh.ma_khach_hang;
